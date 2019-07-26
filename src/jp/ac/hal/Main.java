ab1a53a1601092e9478f3a17f9631f173cb4f7c4
ryoki726 <ryooki.0726@gmail.com>
2019-07-26 14:35:50 +0900

    最大値と配列設定

--- src/jp/ac/hal/Main.java
+++ src/jp/ac/hal/Main.java
@@ -1,5 +1,9 @@
 package jp.ac.hal;
 
-public class Main {
+class Prime {
+	public static void main(String[] args) {
+		int max = 100; //素数を探す数の最大値
+		boolean[] a = new boolean[max]; //素数かどうか判定する配列
 
+	}
 }
